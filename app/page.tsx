4cc6a3ea978b36e1a06b6250e788524369d08156
Mouad Lahlal <lahlalmouad@gmail.com>
2024-11-17 21:50:52 +0100

    Fixed pdf viewer, upload popup and started profile page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { Metadata } from "next"
 import Image from "next/image"
-import { Document, Page, Thumbnail } from 'react-pdf'
 import Link from "next/link"
-import { useRouter } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -15,58 +12,23 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { MainNav } from "@/components/custom/main-nav";
-import { pdfjs } from 'react-pdf';
 
 async function getBooks(setBooks: any) {
   const res = await fetch("/api/books");
   let temp = await res.json();
   setBooks(temp.books);
-  console.log(temp);
 }
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.mjs`;
 
 export default function YourBooksPage() {
 
   const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [pageWidth, setPageWidth] = useState(0);
-
-  const router = useRouter();
-
-  function onPageLoadSuccess() {
-    setLoading(false);
-    setPageWidth(document.getElementById("div_pdfs")?.getBoundingClientRect().width || 100);
-  }
 
   useEffect(() => {
     getBooks(setBooks);
   }, []);
 
-  const options = {
-    cMapUrl: "cmaps/",
-    cMapPacked: true,
-    standardFontDataUrl: "standard_fonts/",
-  };
-
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/dashboard-light.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/dashboard-dark.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="hidden dark:block"
-        />
-      </div>
       <div className="hidden flex-col md:flex">
         <MainNav className="mx-6" />
         <div className="flex-1 space-y-4 p-8 pt-6">
@@ -78,9 +40,9 @@ export default function YourBooksPage() {
               books ?
                 books.map((book: {name: string, thumbnail: string, url: string}) => {
                   return (
-                    <Card>
-                      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                        <CardTitle className="text-sm font-medium">
+                    <Card className="w-fit">
+                      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 w-fit">
+                        <CardTitle className="text-sm font-medium w-[250px]">
                           {book.name}
                         </CardTitle>
                       </CardHeader>
@@ -89,43 +51,17 @@ export default function YourBooksPage() {
                           style={{ borderRadius: '13px', overflow: 'hidden', width: '250px', marginTop: '20px' }}
                           id="div_pdfs"
                         >
-                          <Link href={`/pdf-viewer?book=${book.name}`} className="z-20">
+                          <Link href={`/pdf-viewer?book=${book.name}&url=${encodeURIComponent(book.url)}`} className="z-20">
                             <div className="flex justify-center">
                               <Image
                                 src={book.thumbnail}
-                                width={300}
+                                width={250}
                                 height={300}
                                 alt="Book thumbnail"
                               />
-                              {/* <Document
-                                file={{
-                                  url: book.url,
-                                }}
-                                options={options}
-                                className="max-w-full max-h-full z-10"
-                              >
-                                <Thumbnail
-                                  width={pageWidth * 2}
-                                  key={1}
-                                  pageNumber={1}
-                                  onLoadSuccess={onPageLoadSuccess}
-                                  onRenderError={() => setLoading(false)}
-                                  scale={0.5}
-                                  onItemClick={() => router.push(`/pdf-viewer?book=${book}`)}
-                                />
-                              </Document> */}
                             </div>
-                            {/* <Image
-                              src="https://thebanco.it/public/uploads/books/9788808663856.jpg"
-                              objectFit="cover"
-                              width="384"
-                              height="519"
-                              alt="Dashboard"
-                              className="block"
-                            /> */}
                           </Link>
                         </div>
-
                       </CardContent>
                     </Card>
                   )
--- app/pdf-viewer/page.tsx
+++ app/pdf-viewer/page.tsx
@@ -1,14 +1,12 @@
 "use client"
+
 import { pdfjs } from 'react-pdf';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Document, Page } from 'react-pdf';
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import { Button } from '@/components/ui/button';
 import { MainNav } from "@/components/custom/main-nav";
-import { UserNav } from "@/components/custom/user-nav";
-import { Search } from "@/components/custom/search";
-import { IconPlusSquare } from "@/icons/icon-plus-square";
 import { useSearchParams } from 'next/navigation';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.mjs`;
@@ -19,107 +17,78 @@ const options = {
   standardFontDataUrl: "standard_fonts/",
 };
 
+const ControlButton = ({ onClick, disabled, children, className }: any) => (
+  <Button onClick={onClick} disabled={disabled} className={className}>
+    {children}
+  </Button>
+);
+
 export default function PDFViewer() {
   const [numPages, setNumPages] = useState<number>(0);
   const [pageNumber, setPageNumber] = useState<number>(1);
-  const [loading, setLoading] = useState(true);
   const [pageWidth, setPageWidth] = useState(500);
-  const [file, setFile] = useState("./demo.pdf");
+  const [file, setFile] = useState({ url: '' });
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    setFile(searchParams.get('book') || "");
+    setFile({ url: decodeURIComponent(searchParams.get('url') || "") });
   }, []);
 
-  function onDocumentLoadSuccess({
-    numPages: nextNumPages,
-  }: {
-    numPages: number;
-  }) {
+  function onDocumentLoadSuccess({ numPages: nextNumPages }: { numPages: number }) {
     setNumPages(nextNumPages);
-  }
-
-  function onPageLoadSuccess() {
-    setLoading(false);
-  }
-
-  function goToNextPage() {
-    setPageNumber((prevPageNumber) => prevPageNumber + 1);
-  }
-
-  function goToPreviousPage() {
-    setPageNumber((prevPageNumber) => prevPageNumber - 1);
   }
 
   return (
     <>
       <MainNav className="mx-6" />
 
-      <div className="flex justify-center mx-auto z-20 my-2 w-[90%] h-[85vh] overflow-auto bg-slate-800 rounded-lg">
-        <Document
-          // file={file}
-          file={{
-            url: 'http://api.minio.mlahlal.duckdns.org/unibook/474065119d3f67c1dcaacd1ad505c134.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=admin%2F20241112%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241112T232036Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=6a319304bb4fb4fbf03be5f53636e8cb213701442537a20cea4b87fca7b2571e',
-          }}
-          onLoadSuccess={onDocumentLoadSuccess}
-          options={options}
-          className=""
-        >
-          <Page
-            className="max-w-[calc(100%-3em)]"
-            key={pageNumber}
-            pageNumber={pageNumber}
-            renderAnnotationLayer={true}
-            renderTextLayer={true}
-            onLoadSuccess={onPageLoadSuccess}
-            onRenderError={() => setLoading(false)}
-            width={pageWidth}
-          />
-        </Document>
-      </div>
-      <div className="fixed bottom-0 inset-x-0 max-w-max mx-auto z-30 rounded-t-lg bg-slate-800 px-6 py-4">
-        <Button
-          onClick={goToPreviousPage}
-          disabled={pageNumber <= 1}
-          className="px-2 mr-6"
-        >
-          <span className="">&lt;</span>
-        </Button>
-        <Button
-          onClick={() => {
-            if (pageWidth > 200) {
-              setPageWidth(pageWidth - 100);
-            }
-          }}
-          disabled={pageWidth < 200}
-          className="px-2 mr-4"
-        >
-          <span className="">zoom -</span>
-        </Button>
-        <a href={file} download={file}>
-          <Button>
-            Download
-          </Button>
-        </a>
-        <Button
-          onClick={() => {
-            if (pageWidth + 200 < window.innerWidth) {
-              setPageWidth(pageWidth + 100);
-            }
-          }}
-          disabled={pageWidth + 200 > window.innerWidth}
-          className="px-2 ml-4"
-        >
-          <span className="">zoom +</span>
-        </Button>
-        <Button
-          onClick={goToNextPage}
-          disabled={pageNumber >= numPages!}
-          className="px-2 ml-6"
-        >
-          <span className="">&gt;</span>
-        </Button>
-      </div>
+      {
+        file && (
+          <>
+            <div className="flex justify-center mx-auto z-20 my-2 w-[90%] h-[85vh] overflow-auto bg-slate-800 rounded-lg">
+              <Document
+                file={file}
+                onLoadSuccess={onDocumentLoadSuccess}
+                options={options}
+                className=""
+              >
+                <Page
+                  className="max-w-[calc(100%-3em)]"
+                  key={pageNumber}
+                  pageNumber={pageNumber}
+                  renderAnnotationLayer={false}
+                  renderTextLayer={true}
+                  width={pageWidth}
+                />
+              </Document>
+            </div>
+            
+            <div className="fixed bottom-0 inset-x-0 max-w-max mx-auto z-30 rounded-t-lg bg-slate-800 px-6 py-4">
+              <ControlButton onClick={() => setPageNumber(pageNumber - 1)} disabled={pageNumber <= 1}>
+                &lt;
+              </ControlButton>
+              <ControlButton
+                onClick={() => setPageWidth(pageWidth - 100)}
+                disabled={pageWidth <= 200}
+              >
+                Zoom -
+              </ControlButton>
+              <a href={file.url} download>
+                <Button>Download</Button>
+              </a>
+              <ControlButton
+                onClick={() => setPageWidth(pageWidth + 100)}
+                disabled={pageWidth + 200 >= window.innerWidth}
+              >
+                Zoom +
+              </ControlButton>
+              <ControlButton onClick={() => setPageNumber(pageNumber + 1)} disabled={pageNumber >= numPages}>
+                &gt;
+              </ControlButton>
+            </div>
+          </>
+        )
+      }
     </>
   );
 }
