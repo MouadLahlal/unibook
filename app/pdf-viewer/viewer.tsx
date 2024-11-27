ca78f960e47ff6cd061d986ae6650441f3eef11c
Mouad Lahlal <lahlalmouad@gmail.com>
2024-11-27 21:14:10 +0100

    Fixed minor issues

--- app/pdf-viewer/viewer.tsx
+++ app/pdf-viewer/viewer.tsx
@@ -6,7 +6,7 @@ import { Document, Page } from "react-pdf";
 import { Button } from "@/components/ui/button";
 import { MainNav } from "@/components/custom/main-nav";
 import { useSearchParams } from "next/navigation";
-// import "react-pdf/dist/Page/TextLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 // import "react-pdf/dist/Page/AnnotationLayer.css";
 // import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
